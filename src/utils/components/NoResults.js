caa58c99090a76e5f6ac4ffff0f0d5b54284240b
eleanor <ec15613@my.bristol.ac.uk>
2020-06-06 17:57:50 +0100

    Feat(Styling): NoResults text styling

--- src/utils/components/NoResults.js
+++ src/utils/components/NoResults.js
@@ -1,22 +1,34 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+import {FONT_FAMILY, FONT_SIZE_16, FONT_WEIGHT_BOLD} from 'assets/fonts';
+import {WHITE} from 'assets/colours';
 
 const NoResults = ({numFilters, type}) => {
   return (
     <View>
-      <Text>No {type} found!</Text>
+      <Text style={styles.text}>No {type} found!</Text>
       {numFilters === 1 && (
-        <Text>
-          You currently have {numFilters} filter applied, try removing this!
+        <Text style={styles.text}>
+          You currently have {numFilters} filter applied, try removing this
         </Text>
       )}
       {numFilters > 1 && (
-        <Text>
-          You currently have {numFilters} filters applied, try removing these!
+        <Text style={styles.text}>
+          You currently have {numFilters} filters applied, try removing these
         </Text>
       )}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  text: {
+    fontFamily: FONT_FAMILY,
+    fontSize: FONT_SIZE_16,
+    fontWeight: FONT_WEIGHT_BOLD,
+    color: WHITE,
+    textAlign: 'center',
+  },
+});
 
 export default NoResults;
